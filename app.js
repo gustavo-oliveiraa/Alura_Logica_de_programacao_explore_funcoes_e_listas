88c70f934e8f41caa9e5098d5fc7eae1af970576
Gustavo Maxwel <maxwelgustavo@gmail.com>
2024-02-05 15:23:44 -0300

    Projeto pronto!

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ let tentativas = 1;
 function exebirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', { rate: 1.2 });
 }
 
 function exibirMensagemInicial() {
